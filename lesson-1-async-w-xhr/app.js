aec3f161e0df0d80c11105e9d101ac2339d9af1f
Luca Berton <mr.evolution85@gmail.com>
2018-03-15 12:55:33 +0100

    Add search on NYT articles via API request

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -30,10 +30,33 @@
 				<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
 			   </figure>`
 		   } else {
-			   htmlContent = '<div class="error-no-image">No images available</div>'
+			   htmlContent = '<div class="error-no-image">No images available</div>';
 		   }
-		   
+
 		   responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+		}
+
+		const articleRequest = new XMLHttpRequest();
+		articleRequest.onload = addArticles;
+		articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=50eb5009d808455ab2d7cfbf905d3eab`);
+		articleRequest.send();
+
+		function addArticles () {
+			let htmlContent = '';
+
+			const data = JSON.parse(this.responseText);
+
+			if(data.response && data.response.docs && data.response.docs.length > 1) {
+				htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+							<h2><a href="${article.web_url}">${article.headline.main}</a>S</h2>
+							<p>${article.snippet}</p>
+						</li>`
+				).join('') + '</ul>';
+			} else {
+				htmlContent = '<div class="error-no-articles">No articles available</div>';
+			}
+
+			responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
 		}
     });
 })();
