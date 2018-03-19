cc2aefb14fc9a167dad45f1f2bcdd0c1f608a109
Luca Berton <mr.evolution85@gmail.com>
2018-03-19 12:06:15 +0100

    Lesson 3: retrive image with FetchAPI

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -9,4 +9,28 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
     });
+
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}` ,{
+		headers: {
+			Authorization: 'Client-ID bab5f865d344f1a8e2f3035a7c166fb8ef0b4da2832bfda84495b1d212c4bdc0'
+		}
+	}).then(response => response.json())
+	.then(addImage);
+
+	function addImage(data) {
+		let htmlContent = '';
+		const firstImage = data.results[0];
+
+		if (firstImage) {
+			htmlContent = `<figure>
+				<img src="${firstImage.urls.small}" alt="${searchedForText}">
+				<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+			</figure>`;
+		} else {
+			htmlContent = 'Unfortunately, no image was returned for your search.'
+		}
+
+		responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+	}
+
 })();
